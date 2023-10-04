58ff7158290bd7d26a8105a8569160570195b4f3
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2023-10-03 19:43:56 -0500

    Adding packages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import LoginForm from '@/components/login/LoginForm';
+
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { TextField, Container, Box, Button } from "@mui/material";
+import { FormControl, InputLabel, OutlinedInput } from "@mui/material";
+import { InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const LoginForm = () => {
+
+};
+
+export default LoginForm;
